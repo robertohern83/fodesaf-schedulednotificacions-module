b53179cba4c38d588d079fa122b7b21361d81c9e
robertohernandez <robertohernandez@localhost>
2019-12-08 16:25:13 -0600

    Ajustes a archivos en repositorio de fuentes

--- src/main/java/com/fodesaf/scheduledtask/module/notifications/campaign/NotificationCampaign1.java
+++ src/main/java/com/fodesaf/scheduledtask/module/notifications/campaign/NotificationCampaign1.java
@@ -1,8 +1,6 @@
 package com.fodesaf.scheduledtask.module.notifications.campaign;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.HashMap;
